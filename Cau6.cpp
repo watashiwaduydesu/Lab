c0e24ee8773b10c5c26895d005e10afcefb83ea2
watashiwaduydesu <nguyen.duy0403@gmail.com>
2017-03-28 16:39:09 +0700

    Update Cau6.cpp

--- Cau6.cpp
+++ Cau6.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 void main()
 {
-//	typedef enum menu {NHAP_HH=1, TIM_HH, IN_DS_HH, XOA_HH, CAP_NHAT_HH, LUU_DL, TAI_DL, THOAT};
-	unsigned char choice;
+	typedef enum menu {NHAP_HH=1, TIM_HH, IN_DS_HH, XOA_HH, CAP_NHAT_HH, LUU_DL, TAI_DL, THOAT};
+	menu choice;
 
 	printf("1. Nhap hang hoa\n");
 	printf("2. Tim hang hoa\n");
@@ -14,18 +14,18 @@ void main()
 	printf("6. Luu du lieu\n");
 	printf("7. Tai du lieu\n");
 	printf("8. Thoat\n");
-	cout << "Hay chon chuc nang:"; cin>>choice;
-	cout << choice<<endl;
+	printf("Hay chon chuc nang:");
+	scanf_s("%d", &choice);
 	switch (choice)
 	{
-	case '1': cout << "Nhap hang hoa\n"; break;
-	case '2': cout << "Tim hang hoa\n"; break;
-	case '3': cout << "In ra danh sach hang hoa\n"; break;
-	case '4': cout << "Xoa hang hoa\n"; break;
-	case '5': cout << "Cap nhat hang hoa\n"; break;
-	case '6': cout << "Luu du lieu\n"; break;
-	case '7': cout << "Tai du lieu\n"; break;
-	case '8': cout << "Thoat\n"; break;
+	case NHAP_HH: cout << "Nhap hang hoa\n"; break;
+	case TIM_HH: cout << "Tim hang hoa\n"; break;
+	case IN_DS_HH: cout << "In ra danh sach hang hoa\n"; break;
+	case XOA_HH: cout << "Xoa hang hoa\n"; break;
+	case CAP_NHAT_HH: cout << "Cap nhat hang hoa\n"; break;
+	case LUU_DL: cout << "Luu du lieu\n"; break;
+	case TAI_DL: cout << "Tai du lieu\n"; break;
+	case THOAT: cout << "Thoat\n"; break;
 	default: cout << "Loi nhap\n"; break;
 	}
 
