e40ba33c4d470ade88b372c68066b78c756143b5
watashiwaduydesu <nguyen.duy0403@gmail.com>
2017-04-01 16:35:04 +0700

    Create Cau1.cpp

---
+++ Cau1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	char ch=0;
+	for (int i = 0; i <= 127; i++)
+	{
+		cout << (char)ch << endl;
+		ch++;
+	}
+	system("pause");
+	system("clr");
+}
