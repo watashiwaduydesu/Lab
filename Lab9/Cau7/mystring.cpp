bbccdbb2f36b325028fda6a6b5cf29a3ab5e08af
watashiwaduydesu <nguyen.duy0403@gmail.com>
2017-05-30 19:54:26 +0700

    Update mystring.cpp

--- Lab9/Cau7/mystring.cpp
+++ Lab9/Cau7/mystring.cpp
@@ -33,3 +33,21 @@ MyString& MyString::operator+(MyString& x) {
 	strcat_s(res.m_pchar, 50, x.m_pchar);
 	return res;
 };
+MyString& MyString::operator+(MyString& x) {
+	size_t t = strlen(MyString::m_pchar)+strlen(x.m_pchar);
+	char * res_char= new char[t];
+	MyString* res = new MyString(res_char);
+	strcpy_s(res->m_pchar,t+1,this->m_pchar);
+	strcat_s(res->m_pchar, t + 1, x.m_pchar);
+	return *res;
+	delete res_char;
+};
+MyString& MyString::operator+(const char* x) {
+	size_t t = strlen(MyString::m_pchar) + strlen(x);
+	char * res_char = new char[t];
+	MyString* res = new MyString(res_char);
+	strcpy_s(res->m_pchar, t + 1, this->m_pchar);
+	strcat_s(res->m_pchar, t + 1, x);
+	return *res;
+	delete res_char;
+};
